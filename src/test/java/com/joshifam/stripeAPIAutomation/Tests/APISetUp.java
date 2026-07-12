2af87b95b5e521e8e02fa7d17baad6be71ba69c6
Vijay Joshi <joshivu@gmail.com>
2026-07-11 20:16:43 -0400

    added envv inside the print satement

--- src/test/java/com/joshifam/stripeAPIAutomation/Tests/APISetUp.java
+++ src/test/java/com/joshifam/stripeAPIAutomation/Tests/APISetUp.java
@@ -32,7 +32,7 @@ public class APISetUp {
 	
 	@BeforeSuite
 	public static void beforeSuite() {
-		String envv = System.getProperty("envv");
+		String envv = System.getProperty("environment");
 		System.out.println("\n 1. BEFORE SUITE Starting the Test Eexcution now \n"+System.getProperty("baseURI")+"\n"+envv);
 		RestAssured.baseURI = (cp.getbaseURL()!=null)? cp.getbaseURL():System.getProperty("baseURI");
 		RestAssured.basePath = (cp.getbasePath()!=null)? cp.getbasePath():System.getProperty("basePath");
