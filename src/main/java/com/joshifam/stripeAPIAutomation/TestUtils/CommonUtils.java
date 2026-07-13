1f1b12a973c47cb4334820a1cf340b7c1c6fabf0
Vijay Joshi <joshivu@gmail.com>
2026-07-13 02:58:01 -0400

    corrected the @Sources on the ConfigProperty file

--- src/main/java/com/joshifam/stripeAPIAutomation/TestUtils/CommonUtils.java
+++ src/main/java/com/joshifam/stripeAPIAutomation/TestUtils/CommonUtils.java
@@ -11,26 +11,26 @@ import java.util.Date;
 public class CommonUtils {
 	
 	public static void archiveReport() {
-		String oldreportName = "TestReport.html";
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY_MM_dd_hh_mm_ss");
-		String newReportName = sdf.format(new Date())+"_"+oldreportName;
-		
-		String oldresourcePath = "/src/test/resources/testReports/";
-		String newresourcePath = "/src/test/resources/archivedTestReports/";
-		
-		Path oldFile = Paths.get(System.getProperty("user.dir")+oldresourcePath+oldreportName);
-		Path newPath = Paths.get(System.getProperty("user.dir")+newresourcePath+newReportName);
-		
-		if(!Files.isRegularFile(oldFile)) {
-			  try { Files.createFile(oldFile); }
-			  catch (IOException e) { // TODO Auto-generated catch block 
-			  e.printStackTrace();
-			  }			 
-		 }
-		  try { Files.move(oldFile, newPath, StandardCopyOption.ATOMIC_MOVE); }
-		  	catch (IOException e) { // TODO Auto-generated catch block 
-			  e.printStackTrace(); 
-		  	}	
+	    String oldreportName = "TestReport.html";
+	    SimpleDateFormat sdf = new SimpleDateFormat("YYYY_MM_dd_hh_mm_ss");
+	    String newReportName = sdf.format(new Date()) + "_" + oldreportName;
+	    
+	    String userDir = System.getProperty("user.dir");
+	    
+	    // Let Java construct the platform-safe paths
+	    Path oldFile = Paths.get(userDir, "src", "test", "resources", "testReports", oldreportName);
+	    Path newPath = Paths.get(userDir, "src", "test", "resources", "archivedTestReports", newReportName);
+	    
+	    // Ensure parent directories exist before creating files
+	    try {
+	        Files.createDirectories(newPath.getParent());
+	        if (!Files.isRegularFile(oldFile)) {
+	            Files.createFile(oldFile);
+	        }
+	        Files.move(oldFile, newPath, StandardCopyOption.REPLACE_EXISTING); 
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }   
 	}
 
 }
