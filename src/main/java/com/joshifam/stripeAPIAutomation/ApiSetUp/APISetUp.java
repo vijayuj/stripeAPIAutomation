559187258c6110de45d17d3d3f7e95d77ad3e837
Vijay Joshi <joshivu@gmail.com>
2026-07-13 03:27:47 -0400

    corrected the @Sources on the ConfigProperty file

--- src/main/java/com/joshifam/stripeAPIAutomation/ApiSetUp/APISetUp.java
+++ src/main/java/com/joshifam/stripeAPIAutomation/ApiSetUp/APISetUp.java
@@ -57,17 +57,18 @@ public class APISetUp {
 		 */
 		
 		
-		/*
-		 * CommonUtils.archiveReport();
-		 * 
-		 * 
-		 * excel = new ExcelReader(Paths.get(baseDirectory, cp.getTestData()).toFile());
-		 * 
-		 * File testReport = Paths.get(baseDirectory, cp.getTestReportFilePath(),
-		 * cp.getTestReportName()).toFile(); // ✅ Converted to File
-		 * 
-		 * extentReport = ExtentManager.GetExtent(testReport);
-		 */
+		
+		  CommonUtils.archiveReport();
+		  
+		  
+			/*
+			 * excel = new ExcelReader(Paths.get(baseDirectory, cp.getTestData()).toFile());
+			 * 
+			 * File testReport = Paths.get(baseDirectory, cp.getTestReportFilePath(),
+			 * cp.getTestReportName()).toFile(); // ✅ Converted to File
+			 * 
+			 * extentReport = ExtentManager.GetExtent(testReport);
+			 */
 		 
 		 
 		System.out.println("\n 1. BEFORE SUITE Starting the Test Execution now \n");
